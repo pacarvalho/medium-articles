45d7dde66f00247d2d5832e46f3990b3f4474113
pacarvalho <admin@avantsoft.com.br>
2020-04-23 14:24:23 -0400

    First commit of MATLAB OpenAI Gym Trainer

---
+++ reinforcement-learning/openai-gym-matlab/OpenAIWrapper.m
@@ -0,0 +1,45 @@
+classdef OpenAIWrapper < rl.env.MATLABEnvironment
+    %OPENAIWRAPPER: Wrapper for Python OpenAI Gym for RL trainig in MATLAB
+    
+    properties
+        open_env = py.gym.make('CartPole-v1'); % Select environment name here  
+    end
+
+    %% Necessary Methods
+    methods              
+        % Contructor method creates an instance of the environment
+        function this = OpenAIWrapper()
+            % Initialize Observation settings
+            ObservationInfo = rlNumericSpec([4 1]);
+            ObservationInfo.Name = 'CartPoleObservation';
+            ObservationInfo.Description = 'Position,Velocity,Angle,VelocityAtTip';
+            
+            % Continous Action Specification
+            ActionInfo = rlFiniteSetSpec([0 1]); 
+            ActionInfo.Name = 'PushingPole';
+            
+            % The following line implements built-in functions of RL env
+            this = this@rl.env.MATLABEnvironment(ObservationInfo,ActionInfo);
+        end
+        
+        % Apply system dynamics and simulates the environment with the 
+        % given action for one step.
+        function [Observation,Reward,IsDone,LoggedSignals] = step(this,Action)
+            % Call OpenAI Step Function
+            result = this.open_env.step(int16(Action));
+            
+            % Collect Results
+            Observation = double(result{1})';
+            Reward = result{2};
+            IsDone = result{3};
+            LoggedSignals = [];
+                 
+        end
+        
+        % Reset environment to initial state and output initial observation
+        function InitialObservation = reset(this)
+            result = this.open_env.reset();
+            InitialObservation = double(result)';
+        end
+    end
+end
---
+++ reinforcement-learning/openai-gym-matlab/RLBuiltInExample.m
@@ -0,0 +1,72 @@
+%% OpenAI Cart-Pole Solver 
+% By Paulo Carvalho
+% Based on: https://www.mathworks.com/help/reinforcement-learning/ug/train-dqn-agent-to-balance-cart-pole-system.html
+clear;clc;
+
+%% Script settings
+doTraining = true; % If set to true, the script performs training
+
+%% Create the environment for training
+env = rlPredefinedEnv("CartPole-Discrete");
+
+rng(0);
+
+%% Define the neural networks 
+statePath = [
+    imageInputLayer([4 1 1],'Normalization','none','Name','state')
+    fullyConnectedLayer(24,'Name','CriticStateFC1')
+    reluLayer('Name','CriticRelu1')
+    fullyConnectedLayer(24,'Name','CriticStateFC2')];
+actionPath = [
+    imageInputLayer([1 1 1],'Normalization','none','Name','action')
+    fullyConnectedLayer(24,'Name','CriticActionFC1')];
+commonPath = [
+    additionLayer(2,'Name','add')
+    reluLayer('Name','CriticCommonRelu')
+    fullyConnectedLayer(1,'Name','output')];
+criticNetwork = layerGraph(statePath);
+criticNetwork = addLayers(criticNetwork, actionPath);
+criticNetwork = addLayers(criticNetwork, commonPath);    
+criticNetwork = connectLayers(criticNetwork,'CriticStateFC2','add/in1');
+criticNetwork = connectLayers(criticNetwork,'CriticActionFC1','add/in2');
+
+criticOpts = rlRepresentationOptions('LearnRate',0.01,'GradientThreshold',1);
+
+%% Define the learning agent
+obsInfo = getObservationInfo(env);
+actInfo = getActionInfo(env);
+critic = rlQValueRepresentation(criticNetwork,obsInfo,actInfo,'Observation',{'state'},'Action',{'action'},criticOpts);
+
+agentOpts = rlDQNAgentOptions(...
+    'UseDoubleDQN',false, ...    
+    'TargetUpdateMethod',"periodic", ...
+    'TargetUpdateFrequency',4, ...   
+    'ExperienceBufferLength',100000, ...
+    'DiscountFactor',0.99, ...
+    'MiniBatchSize',256);
+
+agent = rlDQNAgent(critic,agentOpts);
+
+trainOpts = rlTrainingOptions(...
+    'MaxEpisodes', 1000, ...
+    'MaxStepsPerEpisode', 500, ...
+    'Verbose', false, ...
+    'Plots','training-progress',...
+    'StopTrainingCriteria','AverageReward',...
+    'StopTrainingValue',480); 
+
+
+%% Perform training (if required) or load a saved agent
+if doTraining    
+    % Train the agent.
+    trainingStats = train(agent,env,trainOpts);
+else
+    % Load pretrained agent for the example.
+    load('MATLABCartpoleDQN.mat','agent');
+end
+
+%% Simulate the environment with the trained agent
+simOptions = rlSimulationOptions('MaxSteps',500);
+experience = sim(env,agent,simOptions);
+
+totalReward = sum(experience.Reward)
